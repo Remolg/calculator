dee6c143be0580b7c1864106ba2931395c8ec1c2
Remolg <sadikahmet25@gmail.com>
2023-05-07 04:22:06 +0300

    last

--- assets/calculator.js
+++ assets/calculator.js
@@ -2,25 +2,50 @@ let yaz = document.querySelector('.sonuc')
 
 
 
-for(let i = 0; i<=9; i++) {
-    document.querySelector('#number' + i).addEventListener('click' , function() {
+for (let i = 0; i <= 9; i++) {
+    document.querySelector('#number' + i).addEventListener('click', function () {
         yaz.value += this.innerText;
-        
+
     })
 }
-    
-document.querySelector('#division').addEventListener('click' , function() {
-    yaz.value += this.innerText;});
 
-document.querySelector('#multiplication').addEventListener('click' , function() {
-    yaz.value += this.innerText;});
+const toplaBtn = document.querySelector('#plus');
+const temizleBtn = document.querySelector('#ac');
+const bolBtn = document.querySelector('#division');
+const carpBtn = document.querySelector('#multiplication');
+const eksiBtn = document.querySelector('#extraction');
+const hesaplaBtn = document.querySelector('#equal');
+const yuzdeBtn = document.querySelector('#percent');
+const noktaBtn = document.querySelector('#point');
 
-document.querySelector('#extraction').addEventListener('click' , function() {
-    yaz.value += this.innerText;});
+bolBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
 
-document.querySelector('#equal').addEventListener('click' , function() {
-    yaz.value += this.innerText;});
+carpBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
 
-document.querySelector('#ac').addEventListener('click', function() {
-    yaz.value = ' '
-})  
+eksiBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
+
+toplaBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
+
+hesaplaBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
+
+yuzdeBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
+
+temizleBtn.addEventListener('click', function () {
+    yaz.value = ' ';
+});
+
+noktaBtn.addEventListener('click', function () {
+    yaz.value += this.innerText;
+});
